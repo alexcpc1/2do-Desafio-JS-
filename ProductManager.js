d76752c73509be2d30619ae738f73df13dad9576
aljornacpc1 <aljornacpc1@gmail.com>
2023-04-21 00:49:53 -0400

    actualizar

--- ProductManager.js
+++ ProductManager.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 class ProductManager {
     constructor(pathName){
         this.path=pathName;
+   
     }
     fileExists(){
         return fs.existsSync(this.path);
@@ -19,7 +20,14 @@ class ProductManager {
         return newId;
     }
 
-    async addProduct (product){
+    async addProduct (title, description, price, thumbnail, code, stock){
+        if(!title || !description || !price || !thumbnail|| !code || !stock){
+        // return console.log("Todos los campos deben ser Obligatorios");
+        }
+        const codeExists =  this.path.some((element) => element.code === code);
+        if(codeExists){
+            return console.log("Ya existe un producto con este Codigo")
+        };
         try{
             if(this.fileExists()){
                 const content = await fs.promises.readFile(this.path, "utf-8");
@@ -129,23 +137,24 @@ const manager = new ProductManager("./products.json");
 
 const functionPrincipal = async()=>{
     try{
-        const productAdded1 = await manager.addProduct({title: "Cereal", descripcion:   "Cereal Infantil Nestum Frutilla - 250GR", price: 6000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20386774-qf9cW0sV-medium.png",  code: "1012", stock: 100});
-        const productAdded2 = await manager.addProduct({title: "Mantequilla", descripcion:   "Mantequilla con Sal - 250 GR", price: 1500, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/4011088-aosxGOr0-medium.jpg",  code: "1014", stock: 20});
-        const productAdded3 = await manager.addProduct({title: "Chorizo", descripcion:  "Chorizo Parrillero - 20 Und", price: 2000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20050239-Oy6zFS-p-medium.jpg",  code: "1016", stock: 30});
-        const productAdded4 = await manager.addProduct({title: "Queso", descripcion:  "Queso Gauda Laminado - 500 GR", price: 4000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20211526-38YL_0zf-medium.jpg",  code: "1012", stock: 300});
-        const productAdded5 = await manager.addProduct({title: "Risotto", descripcion: "Risotto de Champiñones 40Gr", price: 3000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20548165-LhlJB88C-medium.jpg", code: "1010", stock: 40});
-        const productAdded6 = await manager.addProduct({title: "Pasta", descripcion: "pasta larga 200Gr", price: 1000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20548165-LhlJB88C-medium.jpg", code: "1033", stock: 10});
-        console.log("productAdded1; ", productAdded1);
-        const product1 = await manager.getProductById(1);
-        // console.log("product1: ", product1);
-        const product2 = await manager.getProductById(3);
-        // console.log("product2: ", product2);
-        const resultado = await manager.updateProduct(3,{price:4000});
-        // console.log("resultado: ", resultado);
-        const resultado2 = await manager.getProducts();
-        // console.log("resultado2: ", resultado2);
-        const resultadodeleteProduct = await manager.deleteProduct(2);
-        // console.log("resultadodeleteProduct: ", resultadodeleteProduct);
+        // const productAdded1 = await manager.addProduct({title: "Cereal", descripcion:   "Cereal Infantil Nestum Frutilla - 250GR", price: 6000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20386774-qf9cW0sV-medium.png",  code: "1012", stock: 100});
+        // const productAdded2 = await manager.addProduct({title: "Mantequilla", descripcion:   "Mantequilla con Sal - 250 GR", price: 1500, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/4011088-aosxGOr0-medium.jpg",  code: "1014", stock: 20});
+        // const productAdded3 = await manager.addProduct({title: "Chorizo", descripcion:  "Chorizo Parrillero - 20 Und", price: 2000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20050239-Oy6zFS-p-medium.jpg",  code: "1016", stock: 30});
+        // const productAdded4 = await manager.addProduct({title: "Queso", descripcion:  "Queso Gauda Laminado - 500 GR", price: 4000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20211526-38YL_0zf-medium.jpg",  code: "1012", stock: 300});
+        const productAdded5 = await manager.addProduct({title: "Leche", descripcion: "Risotto de Champiñones 40Gr", price: 3000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20548165-LhlJB88C-medium.jpg", code: "1016", stock: 120});
+        // producto imcompleto:
+        // const productAdded6 = await manager.addProduct({title: "Pasta", descripcion: "pasta larga 200Gr", price: 1000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20548165-LhlJB88C-medium.jpg", stock: 10});
+        // console.log("productAdded6; ", productAdded6);
+        // const product1 = await manager.getProductById(1);
+        // // console.log("product1: ", product1);
+        // const product2 = await manager.getProductById(3);
+        // // console.log("product2: ", product2);
+        // const resultado = await manager.updateProduct(3,{price:4000});
+        // // console.log("resultado: ", resultado);
+        // const resultado2 = await manager.getProducts();
+        // // console.log("resultado2: ", resultado2);
+        // const resultado3 = await manager.deleteProduct(7);
+        // console.log("resultado3: ", resultado3 );
         
     } catch(error){
         console.log(error.message);
